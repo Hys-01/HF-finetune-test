5cfee785cad4c5d21ad1d1a344d2017762c6637a
Hys-01 <hbros2004@gmail.com>
2024-02-03 21:02:37 +1100

    experimenting

--- mistral.py
+++ mistral.py
@@ -1,1 +1,9 @@
 # assuming initial experimentation is promising
+
+print('hello world') 
+
+from transformers import AutoTokenizer, AutoModelForCausalLM
+
+tokenizer = AutoTokenizer.from_pretrained("mistralai/Mixtral-8x7B-Instruct-v0.1")
+model = AutoModelForCausalLM.from_pretrained("mistralai/Mixtral-8x7B-Instruct-v0.1")
+print('print after')
